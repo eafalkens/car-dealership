8218483a96fc33ea4214a762bc9f8e15e97c90ba
eafalkens <alicia-fs@outlook.com>
2026-03-18 18:54:55 +0100

    Add CarRepository

---
+++ src/main/java/org/example/cardealership/repository/CarRepository.java
@@ -0,0 +1,6 @@
+package org.example.cardealership.repository;
+
+import org.example.cardealership.entity.Car;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CarRepository extends JpaRepository<Car, Long> {}

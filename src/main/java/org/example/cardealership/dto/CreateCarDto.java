f08a90cfa821371926de0d1566892127a86cd837
eafalkens <alicia-fs@outlook.com>
2026-03-19 14:08:56 +0100

    Add CreateCarDto

---
+++ src/main/java/org/example/cardealership/dto/CreateCarDto.java
@@ -0,0 +1,64 @@
+package org.example.cardealership.dto;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
+public class CreateCarDto {
+
+    @NotBlank
+    private String brand;
+
+    @NotBlank
+    private String model;
+
+    @NotBlank
+    private String description;
+
+    @Positive @Max(2026)
+    private int year;
+
+    @Positive
+    private double price;
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}

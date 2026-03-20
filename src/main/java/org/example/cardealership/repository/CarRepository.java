97241b7592cd4c2e8a856b193cc799653f9f9a3a
eafalkens <alicia-fs@outlook.com>
2026-03-20 13:59:19 +0100

    Update CarRepository with search methods

--- src/main/java/org/example/cardealership/repository/CarRepository.java
+++ src/main/java/org/example/cardealership/repository/CarRepository.java
@@ -1,6 +1,20 @@
 package org.example.cardealership.repository;
 
 import org.example.cardealership.entity.Car;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.stereotype.Repository;
 
-public interface CarRepository extends JpaRepository<Car, Long> {}
+import java.util.List;
+
+@Repository
+public interface CarRepository extends ListCrudRepository<Car, Long> {
+
+    // Find cars by brand
+    List<Car> findByBrand(String brand);
+
+    // Find cars with price less than a certain value
+    List<Car> findByPriceLessThan(double price);
+
+    // Find cars with year greater than a certain value
+    List<Car> findByYearGreaterThan(int year);
+}
